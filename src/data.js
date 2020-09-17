f0c2e5e3a2b86815a913cf2b430faf3aa2553bb1
Vicky Vasilopoulou <vickyvasilopoulou@Vickys-MacBook.local>
2020-09-17 12:19:26 +0100

    add new changes

---
+++ src/data.js
@@ -0,0 +1,25 @@
+import movingCard from './movingCard.png';
+
+
+export default [
+    {
+        image: movingCard,
+        height: 150
+    },
+    {
+        image: movingCard,
+        height: 300
+    },{
+        image: movingCard,
+        height: 300
+    },{
+        image: movingCard,
+        height: 300
+    },{
+        image: movingCard,
+        height: 300
+    },{
+        image: movingCard,
+        height: 300
+    }
+]
